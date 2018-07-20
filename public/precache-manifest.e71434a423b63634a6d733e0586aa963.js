8f02cffa3cea11d62c6096f0fcbc98e3a17782a7
MrTango2 <md@derico.de>
2018-07-20 19:11:01 +0300

    deploy Fr 20. Jul 19:11:01 EEST 2018

--- public/precache-manifest.e71434a423b63634a6d733e0586aa963.js
+++ public/precache-manifest.e71434a423b63634a6d733e0586aa963.js
@@ -1,22 +1,28 @@
 self.__precacheManifest = [
   {
-    "url": "/3cf6adf61054c328b1b0ddcd8f9ce24d.woff"
+    "url": "/1561b424aaef2f704bbd89155b3ce514.woff2"
   },
   {
     "url": "/vendor.6087a122fdaa10d57ea6.bundle.js"
   },
   {
-    "url": "/5136cbe62a63604402f2fedb97f246f8.woff2"
+    "url": "/3cf6adf61054c328b1b0ddcd8f9ce24d.woff"
   },
   {
-    "revision": "f03944cb8a44d12cddca711c9cf8255e",
+    "revision": "b38d3799d0bca715062ab5b7985c2c06",
     "url": "/index.html"
   },
   {
-    "url": "/eed9aab5449cc9c8430d7d258108f602.woff"
+    "url": "/5136cbe62a63604402f2fedb97f246f8.woff2"
+  },
+  {
+    "url": "/e66e8e745d883d35eb4f6758010f5468.png"
   },
   {
     "url": "/ea36cd9a0e9eee97012a67b8a4570d7b.woff"
+  },
+  {
+    "url": "/eed9aab5449cc9c8430d7d258108f602.woff"
   },
   {
     "url": "/cf4d60bc0b1d4b2314085919a00e1724.woff"
@@ -25,7 +31,13 @@ self.__precacheManifest = [
     "url": "/c0f1e4a4fdfb8048c72e86aadb2a247d.woff2"
   },
   {
-    "url": "/app.86375397e27ca50f0bf4.bundle.js"
+    "url": "/app.ea806343c61fa6003a48.bundle.js"
+  },
+  {
+    "url": "/abec3bbf1b636e2a719d549fb4520e93.png"
+  },
+  {
+    "url": "/6096671475b1baa0a275a1bdf747e48c.png"
   },
   {
     "url": "/44b78f142603eb69f593ed4002ed7a4a.woff"
@@ -35,8 +47,5 @@ self.__precacheManifest = [
   },
   {
     "url": "/1f35e6a11d27d2e10d28946d42332dc5.woff2"
-  },
-  {
-    "url": "/1561b424aaef2f704bbd89155b3ce514.woff2"
   }
 ];
