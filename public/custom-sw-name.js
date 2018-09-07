c1e415352abec29db9f7aefbf3767467888875f2
MrTango <md@derico.de>
2018-09-07 16:14:20 +0300

    deploy Fr 7. Sep 16:14:20 EEST 2018

---
+++ public/custom-sw-name.js
@@ -0,0 +1,18 @@
+importScripts("/precache-manifest.b31ccd58fba721042fe5cf4d0e20f73e.js", "/workbox-v3.4.1/workbox-sw.js");
+workbox.setConfig({modulePathPrefix: "/workbox-v3.4.1"});
+
+if (workbox) {
+  workbox.routing.registerRoute(
+    new RegExp("(https?:\/\/gaipa\.org(\/app\/)()?)"),
+    workbox.strategies.staleWhileRevalidate()
+  );
+  workbox.routing.registerRoute(
+    new RegExp("(http?:\/\/localhost:.*(\/app\/)()?)"),
+    workbox.strategies.staleWhileRevalidate()
+  );
+
+  workbox.precaching.precacheAndRoute([]);
+} else {
+  console.log("Workbox didn't load! Offline support will not work.");
+}
+
---
+++ public/precache-manifest.b31ccd58fba721042fe5cf4d0e20f73e.js
@@ -0,0 +1,48 @@
+self.__precacheManifest = [
+  {
+    "url": "/1561b424aaef2f704bbd89155b3ce514.woff2"
+  },
+  {
+    "revision": "09d0b89935d717d62f8975703b15e2b7",
+    "url": "/index.html"
+  },
+  {
+    "url": "/3cf6adf61054c328b1b0ddcd8f9ce24d.woff"
+  },
+  {
+    "url": "/app.895419c5c02488fbbaa2.bundle.js"
+  },
+  {
+    "url": "/eed9aab5449cc9c8430d7d258108f602.woff"
+  },
+  {
+    "url": "/c0f1e4a4fdfb8048c72e86aadb2a247d.woff2"
+  },
+  {
+    "url": "/cf4d60bc0b1d4b2314085919a00e1724.woff"
+  },
+  {
+    "url": "/e66e8e745d883d35eb4f6758010f5468.png"
+  },
+  {
+    "url": "/ea36cd9a0e9eee97012a67b8a4570d7b.woff"
+  },
+  {
+    "url": "/abec3bbf1b636e2a719d549fb4520e93.png"
+  },
+  {
+    "url": "/6096671475b1baa0a275a1bdf747e48c.png"
+  },
+  {
+    "url": "/5136cbe62a63604402f2fedb97f246f8.woff2"
+  },
+  {
+    "url": "/44b78f142603eb69f593ed4002ed7a4a.woff"
+  },
+  {
+    "url": "/3c37aa69cd77e6a53a067170fa8fe2e9.woff2"
+  },
+  {
+    "url": "/1f35e6a11d27d2e10d28946d42332dc5.woff2"
+  }
+];
